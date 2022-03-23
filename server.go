b4065122630125d12a18e2daa08059343f1e6c96
Patrick Quigley <patrick.quigley@thebeansgroup.com>
2022-03-23 07:50:56 +0000

    Split out Get and Post score.

--- server.go
+++ server.go
@@ -8,14 +8,17 @@ import (
 
 // ServeHTTP serves http requests
 func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		s.PostScore(w, r)
+	} else {
+		s.GetScore(w, r)
+	}
+}
+
+// GetScore of a player, write http response
+func (s Server) GetScore(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	score := s.store.getPlayerScore(player)
-
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusCreated)
-		fmt.Fprint(w, "Score Updated")
-		return
-	}
 
 	if score > 0 {
 		fmt.Fprint(w, score)
@@ -23,6 +26,12 @@ func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "Score Not Found")
 	}
+}
+
+// PostScore of a player, write http response
+func (s Server) PostScore(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprint(w, "Score Updated")
 }
 
 // Server player information
