591884c26eb81eb5908504cdc483f7c633b3c6f5
Patrick Quigley <patrick.quigley@thebeansgroup.com>
2022-03-25 10:14:41 +0000

    Add early version of persisted PlayerStore.
    Only implements half the interface, with a hardcoded query. A start with
    a lot of new concepts: the database/sql package and a library to mock it
    for in-memory unit tests.

---
+++ database.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// SQLStore is a PlayerStore backed by a relational database.
+type SQLStore struct {
+	DB *sql.DB
+}
+
+func (db SQLStore) getPlayerScore(name string) int {
+
+	var score int
+	sqlStatement := "SELECT score FROM players WHERE name = 'bob';"
+	err := db.DB.QueryRow(sqlStatement).Scan(&score)
+
+	if err != nil {
+		log.Fatal(err)
+		return 0
+	}
+
+	return score
+}
---
+++ database_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Test_database_getPlayerScore(t *testing.T) {
+	db, mock, _ := sqlmock.New()
+	defer db.Close()
+
+	type args struct {
+		name string
+	}
+	type dbData struct {
+		sql  string
+		rows *sqlmock.Rows
+	}
+	tests := []struct {
+		name   string
+		args   args
+		want   int
+		dbData dbData
+	}{
+		{
+			"player found",
+			args{name: "bob"},
+			1,
+			dbData{
+				sql:  "SELECT score FROM players WHERE name = 'bob';",
+				rows: sqlmock.NewRows([]string{"score"}).AddRow(1),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sqlStore := SQLStore{DB: db}
+			mock.ExpectQuery(tt.dbData.sql).WillReturnRows(tt.dbData.rows)
+
+			if got := sqlStore.getPlayerScore(tt.args.name); got != tt.want {
+				t.Errorf("getPlayerScore() = %v, want %v", got, tt.want)
+			}
+
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+		})
+	}
+}
